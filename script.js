9da131390ef3b7cc8c2548c3632a967c127e72ac
mauroanastasi <mauroanastasi1@gmail.com>
2025-07-14 19:59:50 +0200

    [UPD] snack 1 done

---
+++ script.js
@@ -0,0 +1,25 @@
+// snack 1
+console.log(somma(2, 3))
+
+function somma(a, b) {
+    return a + b
+}
+
+
+
+// -------------------------------------------
+
+const anotherSomma = function (c, d) {
+    return c + d
+}
+
+console.log(anotherSomma(6, 6))
+
+// -------------------------------------------
+
+const sum = (c, d) => {
+    return c + d
+}
+
+console.log(sum(4, 4))
+
